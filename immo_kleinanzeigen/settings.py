a545a433aa8e8c3743b2d60f0c8271aa85268f4b
Hai Long Le <hailongle@icloud.com>
2023-05-27 10:04:24 +0200

    - set robotstxt_obey to false

--- immo_kleinanzeigen/settings.py
+++ immo_kleinanzeigen/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "immo_kleinanzeigen.spiders"
 #USER_AGENT = "immo_kleinanzeigen (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
