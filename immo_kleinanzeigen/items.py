cbbcd9d27756f1d7c4edcf3820dd9411f090e59d
Hai Long Le <hailongle@icloud.com>
2023-05-27 12:27:32 +0200

    - add more fields

--- immo_kleinanzeigen/items.py
+++ immo_kleinanzeigen/items.py
@@ -3,6 +3,7 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 from dataclasses import dataclass
+from datetime import date
 
 
 @dataclass
@@ -10,5 +11,19 @@ class RealEstateItem:
     id: str
     caption: str
     price: float
+    street: str
+    location: str
     area_living: float
     area_plot: float
+    total_rooms: int
+    bedrooms: int
+    bathrooms: int
+    available_from: str
+    house_type: str
+    floors: int
+    year_build: int
+    commission: str
+    date_inserted: date
+    views: int
+    offerer: str
+    offerer_phone_number: str
--- immo_kleinanzeigen/spiders/kleinanzeigen_spider.py
+++ immo_kleinanzeigen/spiders/kleinanzeigen_spider.py
@@ -3,6 +3,13 @@ import scrapy
 from immo_kleinanzeigen.items import RealEstateItem
 
 
+def strip_if_exist(response, selector):
+    return response.css(selector).get().strip() if response.css(selector).get() is not None else ''
+
+def strip_if_exist_else(response, selector, selector2):
+    return response.css(selector).get().strip() if response.css(selector).get() is not None else strip_if_exist(response, selector2)
+
+
 class KleinanzeigenSpider(scrapy.Spider):
     name = "kleinanzeigen"
     start_urls = ["https://www.kleinanzeigen.de/s-anzeige/hier-eigennutzer-und-vermieter-werden-gepflegtes-mfh-am-schoelerberg-/2374061258-208-3125",
@@ -16,8 +23,23 @@ class KleinanzeigenSpider(scrapy.Spider):
         }
         print(detail_map)
         yield RealEstateItem(
-            id=response.css('#viewad-ad-id-box li:nth-child(2)::text').get().strip(),
-            caption=response.css('h1::text').get().strip(),
-            price=response.css('h2[class*="boxedarticle--price"]::text').get().strip(),
-            area_living=detail_map['Wohnfläche'],
-            area_plot=detail_map['Grundstücksfläche'])
+            id=strip_if_exist(response, '#viewad-ad-id-box li:nth-child(2)::text'),
+            caption=strip_if_exist(response, 'h1::text'),
+            price=strip_if_exist(response, 'h2[class*="boxedarticle--price"]::text'),
+            street=strip_if_exist(response, '#street-address::text'),
+            location=strip_if_exist(response, '#viewad-locality::text'),
+            area_living=detail_map.get('Wohnfläche'),
+            area_plot=detail_map.get('Grundstücksfläche'),
+            total_rooms=detail_map.get('Zimmer'),
+            bedrooms=detail_map.get('Schlafzimmer'),
+            bathrooms=detail_map.get('Badezimmer'),
+            available_from=detail_map.get('Verfügbar ab'),
+            house_type=detail_map.get('Haustyp'),
+            floors=detail_map.get('Etagen'),
+            year_build=detail_map.get('Baujahr'),
+            commission=detail_map.get('Provision'),
+            date_inserted=strip_if_exist(response, '#viewad-extra-info span:nth-child(2)::text'),
+            views=strip_if_exist(response, '#viewad-extra-info span:nth-child(1)::text'),
+            offerer=strip_if_exist_else(response, '#viewad-contact .text-force-linebreak a::text', '#viewad-contact .text-force-linebreak::text'),
+            offerer_phone_number=strip_if_exist(response, '#viewad-contact-phone a::text')
+        )
